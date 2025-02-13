cdc833e329a6b56af4351be4c8c316bd08f6bf1e
NIJANTHAN <nijanthan@codelantic.com>
2025-02-13 22:21:53 +0530

    commit 10

--- src/main/java/com/cricket/lane/booking/management/api/controller/AuthController.java
+++ src/main/java/com/cricket/lane/booking/management/api/controller/AuthController.java
@@ -6,14 +6,12 @@ import com.cricket.lane.booking.management.api.dto.RefreshTokenRequest;
 import com.cricket.lane.booking.management.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     private final AuthenticationService service;
 
