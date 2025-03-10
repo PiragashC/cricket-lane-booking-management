05ccdcc47aeb09af7032bad751ade04df043fc0c
NIJANTHAN <nijanthan@codelantic.com>
2025-03-10 19:43:09 +0530

    commit 6

--- src/main/java/com/cricket/lane/booking/management/repository/CricketLaneBookingRepository.java
+++ src/main/java/com/cricket/lane/booking/management/repository/CricketLaneBookingRepository.java
@@ -4,7 +4,6 @@ import com.cricket.lane.booking.management.api.dto.BookingDto;
 import com.cricket.lane.booking.management.api.dto.BookingResponseDto;
 import com.cricket.lane.booking.management.api.dto.LaneDto;
 import com.cricket.lane.booking.management.entity.CricketLaneBooking;
-import com.cricket.lane.booking.management.entity.Lanes;
 import com.cricket.lane.booking.management.enums.BookingStatus;
 import com.cricket.lane.booking.management.enums.BookingType;
 import org.springframework.data.domain.Page;
@@ -14,14 +13,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Set;
 
-public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneBooking,String> {
+public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneBooking, String> {
 
-    @Query("SELECT DISTINCT NEW com.cricket.lane.booking.management.api.dto.LaneDto(l.id, l.laneName,l.lanePrice) " +
+    @Query("SELECT DISTINCT NEW com.cricket.lane.booking.management.api.dto.LaneDto(l.id, l.laneName, l.lanePrice) " +
             "FROM Lanes l " +
             "WHERE l.id NOT IN ( " +
             "    SELECT DISTINCT sl.laneId " +
@@ -29,16 +26,13 @@ public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneB
             "    INNER JOIN CricketLaneBooking cb ON sl.cricketLaneBookingId = cb.id " +
             "    INNER JOIN BookingDates bd ON cb.id = bd.cricketLaneBookingId " +
             "    WHERE bd.bookingDate IN :dates " +
-            "    AND cb.fromTime <= :toTime " +
-            "    AND cb.toTime >= :fromTime " +
+            "    AND NOT (cb.toTime <= :fromTime OR cb.fromTime >= :toTime) " +
             "    AND cb.bookingStatus IN ('SUCCESS', 'PENDING') " +
             ") AND l.isActive = true")
     List<LaneDto> findAvailableLanes(
             @Param("fromTime") LocalTime fromTime,
             @Param("toTime") LocalTime toTime,
-            @Param("dates") List<LocalDate> dates
-    );
-
+            @Param("dates") List<LocalDate> dates);
 
     @Query("SELECT NEW com.cricket.lane.booking.management.api.dto.BookingResponseDto(c.id, CONCAT(c.firstName,' ', c.lastName) AS userName,c.fromTime,c.toTime,c.telephoneNumber,c.bookingPrice,c.bookingStatus,c.bookingTitle) " +
             "FROM CricketLaneBooking c " +
@@ -47,7 +41,7 @@ public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneB
             "WHERE s.laneId = :laneId " +
             "AND b.bookingDate BETWEEN :fromDate AND :toDate " +
             "AND (c.bookingStatus = 'SUCCESS' OR c.bookingStatus = 'PENDING')")
-    List<BookingResponseDto> getAllBookingsForCalenderAdmin(String laneId, LocalDate fromDate,LocalDate toDate);
+    List<BookingResponseDto> getAllBookingsForCalenderAdmin(String laneId, LocalDate fromDate, LocalDate toDate);
 
     @Query("SELECT NEW com.cricket.lane.booking.management.api.dto.BookingResponseDto(c.id,c.fromTime,c.toTime) " +
             "FROM CricketLaneBooking c " +
@@ -56,7 +50,7 @@ public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneB
             "WHERE s.laneId = :laneId " +
             "AND b.bookingDate BETWEEN :fromDate AND :toDate " +
             "AND (c.bookingStatus = 'SUCCESS' OR c.bookingStatus = 'PENDING')")
-    List<BookingResponseDto> getAllBookingsForCalenderUser(String laneId, LocalDate fromDate,LocalDate toDate);
+    List<BookingResponseDto> getAllBookingsForCalenderUser(String laneId, LocalDate fromDate, LocalDate toDate);
 
     @Query("SELECT COUNT(c) > 0 " +
             "FROM CricketLaneBooking c " +
@@ -68,14 +62,14 @@ public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneB
             "AND s.laneId = :laneId")
     Boolean checkLaneFree(@Param("fromTime") LocalTime fromTime,
                           @Param("toTime") LocalTime toTime,
-                          @Param("date") LocalDate date,@Param("laneId") String laneId);
+                          @Param("date") LocalDate date, @Param("laneId") String laneId);
 
     @Query("SELECT c " +
             "FROM CricketLaneBooking c " +
             "WHERE c.fromTime < :time " +
             "AND c.createdDate = :date " +
             "AND c.bookingStatus = 'PENDING'")
-    List<CricketLaneBooking> stillPending(LocalDate date,LocalTime time);
+    List<CricketLaneBooking> stillPending(LocalDate date, LocalTime time);
 
     @Query("SELECT NEW com.cricket.lane.booking.management.api.dto.BookingDto(c.id,CONCAT(c.firstName,' ',c.lastName) AS customerName,b.bookingDate,c.fromTime, c.toTime,l.laneName,c.bookingStatus,c.email,c.telephoneNumber) " +
             "FROM CricketLaneBooking c " +
@@ -100,6 +94,6 @@ public interface CricketLaneBookingRepository extends JpaRepository<CricketLaneB
             "AND (:status IS NULL OR c.bookingStatus = :status) " +
             "ORDER BY c.createdDate DESC")
     Page<BookingDto> getAllBookingPaginationAllType(Pageable pageable, LocalDate fromDate, LocalDate toDate, String laneId,
-                                             BookingStatus status);
+                                                    BookingStatus status);
 
 }
